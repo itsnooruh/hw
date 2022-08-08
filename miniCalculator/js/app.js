4304f125b7f514dd3103624992a4168807109d47
curiousbluepencil <nura@nuras-MacBook-Pro.local>
2022-08-08 13:25:44 +0500

    calculator

---
+++ miniCalculator/js/app.js
@@ -0,0 +1,15 @@
+let name = "nura";
+let age = 19;
+
+let num1 = 5;
+let num2 = 10;
+
+let sum = `Сумма: 5 + 10 = ${num1 + num2}.`;
+let sub = `Разность: 5 - 10 = ${num1 - num2}.`;
+let mult = `Произведение: 5 * 10 = ${num1 * num2}.`;
+let div = `Частное: 5 / 10 = ${num1 / num2}.`;
+
+let phrase = `Привет, меня зовут ${name}, мне ${age}, хотела бы поделиться с вами таким мини калькулятором, для вычисления я использовала значения ${num1} и ${num2}.`;
+
+console.log(phrase);
+console.log(sum, sub, mult, div);
